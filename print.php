11a4b68aecd6f9e14279befdce0b1004bb134061
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-12-14 10:55:41 +0000

    print

---
+++ print.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Hello World with echo <hr>";
+
+    print "Hello Assembler with print <hr>";
+
+    $foo= array("Hello <br>", "array<br>", "used<br>" ,"print_r");
+    print_r ($foo);
+?>
