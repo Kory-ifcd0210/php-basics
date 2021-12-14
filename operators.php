d2618fec52af4e723134a6662cc65233bc7ad3cd
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-12-14 11:56:17 +0000

    operators

---
+++ operators.php
@@ -0,0 +1,20 @@
+<?php
+    echo "<i>The example numbers used are 3 and 5 in that order.</i><br><br>";
+    echo "Arithmetic operators +, -, *, /, and%<br>";
+        var_dump(3+5);
+        var_dump(3-5);
+        var_dump(3*5);
+        var_dump(3/5);
+        var_dump(3%5);
+
+    echo "<hr>";
+    echo "Comparison operators ==,! =, <,>, <=,> =<br>";
+    var_dump(3==5);
+    var_dump(3!=5);
+    var_dump(3>5);
+    var_dump(3<5);
+    var_dump(3<=5);
+    var_dump(3>=5);
+
+
+?>
