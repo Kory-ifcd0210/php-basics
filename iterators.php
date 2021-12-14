91df88fb6d5b00384083a8c36cad8fcc33d5774e
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-12-14 11:15:38 +0000

    iterators

---
+++ iterators.php
@@ -0,0 +1,22 @@
+<?php
+$arr = array(1,2,3,4,5,6,7,8,9);
+echo "Iterator For <br>";
+    for ($i=0; $i < count($arr) ; $i++) {
+        echo "The position is: $i <br>";
+    }
+
+    echo "<hr>";
+    echo "Iterator ForEach <br>";
+    foreach ($arr as &$valor) {
+        echo " {$valor} - ";
+    }
+
+    echo "<hr>";
+    echo "Iterator While <br>";
+    $x = 0;
+
+    while($x <= 100) {
+        echo "The number is: $x <br>";
+        $x+=10;
+    }
+?>
