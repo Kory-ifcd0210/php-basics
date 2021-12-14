65687bf3853c7816eb1f24159521401e8875afb2
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-12-14 13:46:18 +0000

    conditional

--- conditionals.php
+++ conditionals.php
@@ -1,3 +1,28 @@
 <?php
-    
+    $date= new DateTime("2020-01-01");
+    echo "Evaluates whether the current day is Monday <br>";
+    if(date('l')=="monday"){
+        echo "We are on Monday";
+    }else{
+        echo "We aren't on Monday";
+    }
+
+    echo "<hr>";
+    echo "Evaluates whether the current month is October<br>";
+    if(date('m')=="October"){
+        echo "We are on October";
+    }else{
+        echo "We aren't on October";
+    }
+
+    echo "<hr>";
+    echo "Current minutes<br>";
+    if (date('i')<10){
+        echo "The current minute is less than 10";
+    }else if (date('i')>15){
+        echo "The current minute is more than 15";
+    }else {
+        echo "Does not meet any conditions";
+    }
+
 ?>
