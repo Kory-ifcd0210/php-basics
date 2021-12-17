8d17fc260de4834f1d92c8234da7d10c339a673f
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-12-17 09:05:52 +0000

    types

---
+++ types.php
@@ -0,0 +1,14 @@
+<?php
+    echo "Types of variables and data <br>";
+    print_r ($boolean=true);
+    print_r ($int=5);
+
+
+
+    $arrayObj= array(
+        "marca"=> "lg",
+        "model"=> "tesla",
+        "price"=> 14
+    );
+    var_dump($arrayObj);
+?>
